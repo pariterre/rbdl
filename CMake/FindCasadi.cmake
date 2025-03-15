93475e2ea9bc87f37709a2312533ce3187f054b9
Pariterre <pariterre@hotmail.com>
2025-03-15 13:02:39 -0400

    Added Casadi_DIR

--- CMake/FindCasadi.cmake
+++ CMake/FindCasadi.cmake
@@ -5,6 +5,8 @@
 #  Casadi_LIBRARIES   - List of libraries when using Casadi.
 #  Casadi_FOUND       - True if Casadi is found.
 
+set(Casadi_DIR "" CACHE PATH "Path to Casadi installation")
+
 if (Casadi_INCLUDE_DIR)
   # Already in cache, be silent
   set (Casadi_FIND_QUIETLY TRUE)
@@ -12,13 +14,20 @@ endif (Casadi_INCLUDE_DIR)
 
 find_path (Casadi_INCLUDE_DIR "casadi.hpp" 
     PATHS
+    ${Casadi_DIR}/casadi
+    ${Casadi_DIR}/include/casadi
     ${CMAKE_INSTALL_PREFIX}/include/casadi
     ${CMAKE_INSTALL_PREFIX}/Library/include/casadi
     /usr/local/include/casadi
     /usr/include/casadi
 )
 find_library (Casadi_LIBRARY NAMES casadi 
-    PATHS ${CMAKE_INSTALL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/Library/lib
+    PATHS 
+    ${Casadi_DIR}
+    ${Casadi_DIR}/lib
+    ${Casadi_DIR}/Library/lib
+    ${CMAKE_INSTALL_PREFIX}/lib 
+    ${CMAKE_INSTALL_PREFIX}/Library/lib
     /usr/lib
 )
 
@@ -29,6 +38,3 @@ find_package_handle_standard_args (Casadi DEFAULT_MSG
     Casadi_LIBRARY
     Casadi_INCLUDE_DIR
 )
-
-
-
